31aba2060d9345bd9e1cc6ff1b37cc5bb598bcf5
omarlshenawy <omar.bek1122@gmail.com>
2026-08-11 07:55:47 +0300

    Fix Flutter web Cloudflare deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+set -e
+
+echo "Installing Flutter..."
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
 
 export PATH="$HOME/flutter/bin:$PATH"
 
+echo "Flutter version:"
 flutter --version
 
+echo "Getting dependencies..."
 flutter pub get
 
+echo "Building Flutter Web..."
 flutter build web --release
+
+echo "Flutter Web build completed!"
